a1f182c5e9ebc9d87c42827e27a35e20fb379ce0
Natanael Pacheco <natepacheco716@gmail.com>
2023-01-23 02:27:34 -0600

    Implemented Navbar component.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{html,js,jsx}",
+    "./components/**/*.{html,js,jsx}",
+    "./sections/**/*.{html,js,jsx}",
+    "./styles/**/*.{js,jsx}",
     "./node_modules/flowbite-react/**/*.js",
   ],
+  plugins: [require("flowbite/plugin")],
+  mode: "jit",
   theme: {
     extend: {
-      plugins: [require("flowbite/plugin")],
-      theme: {
-        extend: {
-          colors: {
-            "primary-black": "#020B18",
-            "secondary-white": "#c7c7c7",
-            "main-900": "#190A01",
-            "main-800": "#632903",
-            "main-700": "#943E05",
-            "main-600": "#C65306",
-            "main-500": "#F76809",
-            "main-400": "#F98639",
-            "main-300": "#FAA46B",
-            "main-200": "#FCC29C",
-            "main-100": "#FDE1CE",
-          },
-        },
+      colors: {
+        "primary-black": "#18181C",
+        "main-950": "#111A00",
+        "main-900": "#213300",
+        "main-800": "#426600",
+        "main-700": "#639900",
+        "main-600": "#85CC00",
+        "main-500": "#ADFF15",
+        "main-400": "#B8FF33",
+        "main-300": "#C9FF66",
+        "main-200": "#DBFF99",
+        "main-100": "#EDFFCC",
       },
     },
   },
